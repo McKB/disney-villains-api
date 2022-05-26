bb87e1f395cc61897e78d930e4b88ebad63231a6
McKB <mckennakb@comcast.net>
2022-05-25 22:25:20 -0400

    adds error handling to villains.js controller to pass all tests

--- controllers/villains.js
+++ controllers/villains.js
@@ -1,35 +1,47 @@
 const { villains } = require('../models/index')
 
 const getAllVillains = async (req, res) => {
-  const allVillains = await villains.findAll()
+  try {
+    const allVillains = await villains.findAll()
 
-  return res.status(200).send(allVillains)
+    return res.status(200).send(allVillains)
+  } catch (error) {
+    return res.sendStatus(500)
+  }
 }
 
 const getBySlug = async (req, res) => {
-  const searchedSlug = req.params.slug
+  try {
+    const searchedSlug = req.params.slug
 
-  const villain = await villains.findOne({
-    where: { slug: searchedSlug }
-  })
+    const villain = await villains.findOne({
+      where: { slug: searchedSlug }
+    })
 
-  if (villain) {
-    return res.status(200).send(villain)
-  } else {
-    return res.status(404).send('Sorry, the Queen of Hearts chopped this one\'s head off. We have no record of them now. Try again!') // eslint-disable-line max-len
+    if (villain) {
+      return res.status(200).send(villain)
+    } else {
+      return res.status(404).send('Sorry, the Queen of Hearts chopped this one\'s head off. We have no record of them now. Try again!') // eslint-disable-line max-len
+    }
+  } catch (error) {
+    return res.sendStatus(500)
   }
 }
 
 const addVillain = async (req, res) => {
-  const { name, movie, slug } = req.body
+  try {
+    const { name, movie, slug } = req.body
 
-  if (!name || !movie || !slug) {
-    return res.status(400).send('We gotta know our enemies. Need a name, movie, and slug, please!')
+    if (!name || !movie || !slug) {
+      return res.status(400).send('We gotta know our enemies. Need a name, movie, and slug, please!')
+    }
+
+    const newVillain = await villains.create({ name, movie, slug })
+
+    return res.status(201).send(newVillain)
+  } catch (error) {
+    return res.sendStatus(500)
   }
-
-  const newVillain = await villains.create({ name, movie, slug })
-
-  return res.status(201).send(newVillain)
 }
 
 module.exports = { getAllVillains, getBySlug, addVillain }
